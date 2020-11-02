7c148b2d86e3b28129ecc549c3826e2e3aabb774
Abhishek koushal <akaushal451@gmail.com>
2020-11-02 20:29:59 +0530

    Commit Price changes

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import NavBar from './NavBar';
-import Section from './Section';
 import Footer from './Footer';
 
 
@@ -8,7 +7,6 @@ export default function Price() {
     return (
     <>  
         <NavBar></NavBar>
-        <Section/>
         <h1 className="m-5 p-5 text-center mt-5">OOPs Error 404<br/>Page not Found</h1>
         <Footer></Footer>
     </>
